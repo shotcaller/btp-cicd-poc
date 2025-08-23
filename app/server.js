38173afa822fa8d3eb2c0e87b60d9a8204208035
Ruturaj Ghodke <ruturaj.ghodke@mccain.ca>
2025-08-23 20:40:12 -0300

    set up basic node app

---
+++ app/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World from SAP BTP CF via Github Actions!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
