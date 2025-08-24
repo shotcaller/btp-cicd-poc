5f5881cfd96be18beac3c24489f99813ea92d1ee
Ruturaj Ghodke <ruturaj.ghodke@mccain.ca>
2025-08-23 21:54:29 -0300

    test update

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World from SAP BTP CF via Github Actions!')
+    res.send('Hello World from SAP BTP CF via Github Actions. This is an update!')
 })
 
 app.listen(port, () => {
