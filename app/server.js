4de65eb9ccf2a6d6b3e02746724d85cfc966fd30
Ruturaj Ghodke <ruturaj.ghodke@mccain.ca>
2025-10-16 15:16:25 -0300

    update wf name change

--- app/server.js
+++ app/server.js
@@ -3,19 +3,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  //Return a pretty UI message
-  res.send(`
-    <html>
-      <head>
-        <title>Hello World</title>
-      </head>
-      <body>
-        <h1>Hello World from SAP BTP CF via Github Actions</h1>
-        <p>This is an update!</p>
-      </body>
-    </html>
-  `)
+  //Return a pretty UI message, with good styling
+  res.send('<h1 style="color:blue;text-align:center;">Welcome to the CAP Sample Application!</h1><p style="text-align:center;">This is a simple Express server running on port ' + port + '.</p>')
 })
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
